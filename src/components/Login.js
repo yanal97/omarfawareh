64a3cc18baa4093fc927d6d5707a4f51a00ab8a6
Omar Alfawareh <alfawareho@gmail.com>
2024-01-15 16:06:06 +0300

    Feature: Designed Login Component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,41 @@
-import React from "react";
-
+import React, { useState } from "react";
+import classes from "./Login.module.css";
+import { Input, Button, message } from "antd";
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const HandleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const HandlePassChange = (e) => {
+    setPassword(e.target.value);
+  };
+  function ValidateInput(params) {
+    console.log("VALIDATING");
+  }
+  return (
+    <div className={classes.login}>
+      <div className={classes.card}>
+        <h1>Login</h1>
+        <div style={{ width: "50%" }}>
+          <Input
+            placeholder="Email"
+            htmlType="email"
+            value={email}
+            onChange={HandleEmailChange}
+          />
+          <Input.Password
+            placeholder="Password"
+            value={password}
+            onChange={HandlePassChange}
+          />
+          <Button className={classes.button} onClick={ValidateInput}>
+            Login
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
